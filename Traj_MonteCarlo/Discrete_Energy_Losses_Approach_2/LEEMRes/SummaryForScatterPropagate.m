21741a9bfdb0a4c8f2c4c124fe7694fde5f30b97
jonhsma <jonathanma1000@berkeley.edu>
2018-10-16 17:23:00 -0700

    Right before the run with 40 eV electrons

--- Traj_MonteCarlo/Discrete_Energy_Losses_Approach_2/LEEMRes/SummaryForScatterPropagate.m
+++ Traj_MonteCarlo/Discrete_Energy_Losses_Approach_2/LEEMRes/SummaryForScatterPropagate.m
@@ -8,4 +8,5 @@
 clear;
 for i = 1:5
     load(strcat('Ein=80.00_Dose=2.00epnm2_Ef=15.5_pag-Emin=5_rcnrad=3.00_PAG=0.4_T',num2str(i),'.mat'))
+    spread(i)= sqrt(sum(acid_xyz(:,1).^2 +acid_xyz(:,2).^2)./size(acid_xyz,1));
 end
